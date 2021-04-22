23168ca332e9c89e12e3144a2791603a691dce51
MKortmann <companymvms@gmail.com>
2021-04-22 22:15:19 +0200

    add create todo

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,48 @@
 // import 'source-map-support/register'
 
-// import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import * as AWS from 'aws-sdk';
+import * as uuid from 'uuid';
 
-// import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+// need to write items to DynamoDB
+const docClient = new AWS.DynamoDB.DocumentClient();
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const newTodo: CreateTodoRequest = JSON.parse(event.body)
+// get the todo table name to save the item
+const todosTable = process.env.TODOS_TABLE;
 
-//   // TODO: Implement creating a new TODO item
-//   return undefined
-// }
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+
+  console.log("At create function");
+  // getting the body
+  const parsedBody: CreateTodoRequest = JSON.parse(event.body)
+
+  // creating a new uuid for this Todo Item
+  const id = uuid.v4();
+
+  // new todo
+  const todoItem = {
+    id,
+    ...parsedBody
+  }
+
+  console.log("item created:", todoItem)
+
+  // add this to dynamoDB
+  await docClient.put({
+    TableName: todosTable,
+    Item: todoItem
+  }).promise()
+
+  // Return result
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      todoItem
+    })
+  }
+
+}
