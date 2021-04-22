8cc49b4a170f1237b48cd2c12edae21ab105db04
MKortmann <companymvms@gmail.com>
2021-04-22 17:55:20 +0200

    running on linux from the source of backend

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -2,6 +2,27 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
+import { createLogger } from '../../utils/logger';
+
+const logger = createLogger('getTodos');
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
+  console.log('Processing event: ', event)
+
+  logger.info('Processing getTodos Event: ', {
+    event
+  })
+
+
+    // Return result
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({
+        work: "worked"
+      })
+    }
 }
