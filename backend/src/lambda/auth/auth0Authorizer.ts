97650c73584037f4d34170327e3ca8e86161005c
MKortmann <companymvms@gmail.com>
2021-04-24 22:54:54 +0200

    bug: fix the problem with the signinkey

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -6,22 +6,16 @@
 
 import { CustomAuthorizerEvent, CustomAuthorizerHandler, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
-// import Axios from 'axios'
-
-// import { verify, decode } from 'jsonwebtoken'
-import { verify } from 'jsonwebtoken'
+import Axios from 'axios'
+import { verify, decode } from 'jsonwebtoken'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
-// import { createLogger } from '../../utils/logger'
+import { createLogger } from '../../utils/logger'
 
-// const logger = createLogger('auth');
-// const axios = require('axios');
+const logger = createLogger('auth');
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
-// to verify JWT token signature.
-// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-// const jwksUrl = 'https://dev-oeeh6hdb.eu.auth0.com/.well-known/jwks.json'
+const jwksUrl = 'https://dev-oeeh6hdb.eu.auth0.com/.well-known/jwks.json'
 
 export const handler: CustomAuthorizerHandler = async (
   event: CustomAuthorizerEvent
@@ -31,7 +25,7 @@ export const handler: CustomAuthorizerHandler = async (
     // we first call the verifyToken function and pass the token that we need to verify.
     const jwtToken = await verifyToken(event.authorizationToken)
 
-    console.log('User was authorized')
+    logger.info('User was authorized', {jwtToken: jwtToken});
 
     // if the verifyToken does not throw any expection we will return the policy for
     // API Gateway
@@ -49,7 +43,8 @@ export const handler: CustomAuthorizerHandler = async (
       }
     }
   } catch (e) {
-    console.log('User was not authorized', e.message)
+
+    logger.error('User was not authorized', {error: e.message})
 
     // if the user is not authorized, we return another police that will
     // deny access to any Lambda functions.
@@ -71,37 +66,28 @@ export const handler: CustomAuthorizerHandler = async (
 
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
-  const token = getToken(authHeader)
-  // const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  const token = getToken(authHeader);
+  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  const jwtKid = jwt.header.kid
+  let cert: string | Buffer
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-    // if we do not have an authentication header
-  // const cert = await axios.get(jwksUrl);
-  const cert = `-----BEGIN CERTIFICATE-----
-  MIIDDTCCAfWgAwIBAgIJYfwAsKroa5QnMA0GCSqGSIb3DQEBCwUAMCQxIjAgBgNV
-  BAMTGWRldi1vZWVoNmhkYi5ldS5hdXRoMC5jb20wHhcNMjEwNDEwMTY0MTE1WhcN
-  MzQxMjE4MTY0MTE1WjAkMSIwIAYDVQQDExlkZXYtb2VlaDZoZGIuZXUuYXV0aDAu
-  Y29tMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAq2HjYPnk1hYuYq9a
-  YqPYVndEuJ23MTaEY4to2oV3RkYpkSVwr6jcI8MdxrQFOGkugDXSb19RwdYtfGDR
-  eo1gRp9Esx/ZV+RCDGFOtKf70x5ttqb9TaFt80bIQy+zRBtdqRl/AgRA9aV4TGCf
-  LNMoAQ4ojCwoIEkqjGo1nR83yEzHt3osaI7iI6E3w4vx9CZ8ZiNudV6wIu9qSxsn
-  DG3nPO41aCIq54/BAMQrCt8krlskpLsxD/+E0Er87vDgqM638dF2LM8V1LCk1xxu
-  QdyHd8HTDpOOJsHODycgFom9rFZgBjF65MjIEE4+U61d0kpb7ByIIY1wEdfQTA8P
-  s/0abQIDAQABo0IwQDAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBSPfwzqhxv1
-  h0tWP7tiwT1JYneOgDAOBgNVHQ8BAf8EBAMCAoQwDQYJKoZIhvcNAQELBQADggEB
-  AAygdKvF78lqQ2EWP5/MZ/rWjo44/7LIKFH040M8nU2Vqb2XHn73U/x9tmbzWeyT
-  D7XH/KKuzCOGiZ0RYqkSzJAsF5YGroOWFOu7iNlk09LVZjUEvJoxZ3gqucnP0mfd
-  6h4/3IB9IlEHOebybj0hO96/XAVLA621uGJOUgXh47YtmejlRfKpISeHtV1AOFxX
-  JMzIw06OF6cMUGSGrhOVFjYjqzafycBHw1TIzhTYAtxR/tZ/Sgqf1A+hcXHv2KdE
-  1fZQsbPovRDbivDuCdP9su/SAujb1rJTlU+21h9UsOMxDfNtVGD0Nx8QfOT/mh9p
-  l/sU6e+vUKY8RmPPoswoD8Y=
-  -----END CERTIFICATE-----`
+  try {
+    const jwks = await Axios.get(jwksUrl);
+    const signingKey = jwks.data.keys.filter(k => k.kid === jwtKid)[0];
 
-  // verify the function and return the result as JwtToken (cast operation)
-  return verify(token, cert, {algorithms: ['RS256']}) as Jwt["payload"]
-  // no error, so a request has been authorized
+
+  if (!signingKey) {
+    throw new Error(`Unable to find a signing key that matches '${jwtKid}'`);
+  }
+  const { x5c } = signingKey;
+
+  cert = `-----BEGIN CERTIFICATE-----\n${x5c[0]}\n-----END CERTIFICATE-----`;
+
+  } catch (error) {
+    console.log('Error While getting Certificate : ', error);
+}
+
+return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload;
 
 }
 
