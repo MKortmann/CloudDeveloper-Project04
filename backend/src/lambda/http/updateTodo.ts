ec731b58fd157a70ad25325cc50a1e6b92df8ec5
MKortmann <companymvms@gmail.com>
2021-04-27 21:26:32 +0200

    add: middleware for updateTodo

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -1,38 +1,42 @@
 import 'source-map-support/register'
-import {
-	APIGatewayProxyEvent,
-	APIGatewayProxyHandler,
-	APIGatewayProxyResult
-} from 'aws-lambda'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 import { updatedTodo } from '../../businessLogic/todos'
 import { createLogger } from '../../utils/logger'
 import { getToken } from '../utils'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
 
 const logger = createLogger('updateTodos')
 
-export const handler: APIGatewayProxyHandler = async (
-	event: APIGatewayProxyEvent
-): Promise<APIGatewayProxyResult> => {
-	const todoId = event.pathParameters.todoId
-	const parsedBody: UpdateTodoRequest = JSON.parse(event.body)
+export const handler = middy(
+	async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+		const todoId = event.pathParameters.todoId
+		const parsedBody: UpdateTodoRequest = JSON.parse(event.body)
 
-	logger.info('Getting an item to be updated: ', {
-		event
-	})
-	logger.info('Item to be updated: ', {
-		updatedTodo
-	})
+		logger.info('Getting an item to be updated: ', {
+			event
+		})
+		logger.info('Item to be updated: ', {
+			updatedTodo
+		})
 
-	const jwtToken = getToken(event)
+		const jwtToken = getToken(event)
 
-	const result = await updatedTodo(jwtToken, todoId, parsedBody)
+		const result = await updatedTodo(jwtToken, todoId, parsedBody)
 
-	return {
-		statusCode: result.statusCode,
-		headers: {
-			'Access-Control-Allow-Origin': '*'
-		},
-		body: result.body
+		return {
+			statusCode: result.statusCode,
+			headers: {
+				'Access-Control-Allow-Origin': '*'
+			},
+			body: result.body
+		}
 	}
-}
+)
+
+handler.use(
+	cors({
+		credentials: true
+	})
+)
